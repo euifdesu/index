e36d49ef3730eef417a1eb70d9af2f3703c6a0f4
euifdesu <euifdesu@gmail.com>
2025-06-19 00:12:18 -0300

    Create perdido_no_tempo.js

---
+++ letra/perdido_no_tempo.js
@@ -0,0 +1,19 @@
+const textoPerdidoNoTempo = `
+       Perdido no tempo pensando em você<br>
+    Meus olhos só vêem a escuridão<br>
+    Espero um dia poder entender<br>
+    A luz que clareia seu coração<br>
+    __________( 1 / 5 )<br>
+    <br>
+    A gente só entende o que é o amor<br>
+    Quando alguém nos ensina a amar<br>
+    Nas noites de chuvas que não tem calor<br>
+    Queria um beijo poder te dar<br>
+    __________( 2 / 4 )<br>
+    <br>
+    Estou aqui só, mas eu penso em ti<br>
+    Mas eu não me sinto em solidão<br>
+    É como se você estivesse aqui<br>
+    Guardada dentro do meu coração<br>
+    __________( 3 / 6 )<br>
+`;

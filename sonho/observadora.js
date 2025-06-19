a67d8e1715e0d239c7c0199be8bd5458c0ad8d18
euifdesu <euifdesu@gmail.com>
2025-06-19 00:13:39 -0300

    Create observadora.js

---
+++ sonho/observadora.js
@@ -0,0 +1,40 @@
+const textoCompleto = `
+       Era uma noite com bastante susuros de pessoas
+conversando em frente a uma grande escola que por
+sua vez tinha um Magazine de livros cituada na 
+entrada e na calçada principal.
+este magazine servia como ponto de ônibus para
+as pessoa que dalí saiam.
+
+Enquanto esperavam o ônibus para voltarem para suas casas, havia também um homem 
+mais velho na calçada, este homem conversava muito comigo,
+e me explicava detalhes daquele lugar e exatamente
+tudo o que acontecia alí durante as noites.
+
+O tempo passou rápido e eu me lembrei que tinha
+esquecido um lápis num banco de cimento dentro da
+grande escola, daí eu acendi um cigarro e fui lá
+buscar, a noite fria e o calor do cigarro me proporcionava
+um tál relaxamento profundo que eu nem me importava
+se eu estivesse atrasado para ir pra casa ou não.
+
+Entrei na grande escola então.......
+tinha um quiosque lá dentro que servia os alunos
+que terminavam aulas e queriam curtir mas a noite.
+
+Um pouco mais distantes do quiosque vi uma garota
+mística vestida de preto, havia muitas pessoas naquele
+lugar, eu sentia como se a garota de preto estivesse do
+meu lado de tão forte a presença, porém ela estava a 100
+metros de distancia de mim, mas é como se o silêncio dela
+estivesse me tocanto por dentro como se quisesse me falar
+algo apenas com sua presença quieta e tranquila, 
+e a cada passo que eu dava me sentia como se estivesse sendo observado nos
+mínimos detalhes.
+
+Dái a primeira gota cai lentamente do céu tocando no
+meu braço, e então minha conciência vai desvanescendo
+aos poucos enquanto ela me observava sem tirar sequer os 
+olhos de mim, minha conciência se apagava,
+até não sobrar mais nada.
+`;

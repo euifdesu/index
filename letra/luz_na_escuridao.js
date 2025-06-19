ddd13aa5e0339e40801517a57b2fcf7477a0ca3a
euifdesu <euifdesu@gmail.com>
2025-06-19 00:11:16 -0300

    Create luz_na_escuridao.js

---
+++ letra/luz_na_escuridao.js
@@ -0,0 +1,22 @@
+const textoLuzNaEscuridao = `
+    Escolher entre o bem e o mal<br>
+    Dentro de si haverá a decisão final<br>
+    Entre a sombra e a luz você irá andar<br>
+    Bestas e Anjos buscam te dominar<br>
+    __________( 1 / 3 )<br>
+    <br>
+
+    Há um pouco de luz na escuridão...<br>
+... E um pouco de escuridão na luz<br>
+Depois vê se você consegui decifrar<br>
+Decifrar ( ah ah )<br>
+__________( 2 / 4 / 5 / 7 )<br>
+<br>
+
+Woh... Woh ô-ô-ô-ô-ô-ô<br>
+Woh ô-ô-ô-ô-ô-ô<br>
+Woh ô-ô-ô-ô-ô-ô<br>
+Woh ô-ô-ô......<br>
+__________( 6 )<br>
+
+`;

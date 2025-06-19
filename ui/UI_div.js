ab0f27efe05cf810b38e4c182380b24a21abc3f8
euifdesu <euifdesu@gmail.com>
2025-06-19 00:16:13 -0300

    Create UI_div.js

---
+++ ui/UI_div.js
@@ -0,0 +1,7 @@
+class UI_div {
+  constructor() {
+    this.element                      = document.createElement('div');
+  	this.element.style.position       = 'absolute';
+    ROOT.appendChild(this.element);
+  }
+}
